afcd21780a926c599ad74799286fd98cea636f13
gilmurillo11 <gilmurillo11@gmail.com>
2016-12-17 00:39:54 -0800

    Update create_account.rb

--- features/support/helpers/create_account.rb
+++ features/support/helpers/create_account.rb
@@ -17,13 +17,13 @@ class User
     puts @new_email
 
     find('//*[@id="at-signup-link"]').click
-    expect(page).to have_selector(:id, 'at-spree_user[email]-field')
-    fill_in('at-spree_user[email]-field', :with => @new_email)
-    expect(page).to have_selector(:id, 'at-spree_user[password]-field')
-    fill_in('at-spree_user[password]-field', :with => @new_password)
-    expect(page).to have_selector(:id, 'at-spree_user[password_confirmation]-field')
-    fill_in('at-spree_user[password_confirmation]-field', :with => @new_password)
-    expect(page).to have_text("Get in on top secret Ref news".upcase)
+    expect(page).to have_selector(:id, '[email]-field')
+    fill_in('[email]-field', :with => @new_email)
+    expect(page).to have_selector(:id, 'password-field')
+    fill_in('[password]-field', :with => @new_password)
+    expect(page).to have_selector(:id, 'password_confirmation-field')
+    fill_in('[password_confirmation]-field', :with => @new_password)
+    expect(page).to have_text("Text".upcase)
     find(:id, 'join-button').click
   end
 end
